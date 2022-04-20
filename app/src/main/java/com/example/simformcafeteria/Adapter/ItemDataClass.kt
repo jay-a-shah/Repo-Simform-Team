9ff3e3a5a8c49259d6e046eb7d22103156685987
buddhdev-jay <jay.b@simformsolutions.com>
2022-04-20 10:11:25 +0530

    Adapter Updated

---
+++ app/src/main/java/com/example/simformcafeteria/Adapter/ItemDataClass.kt
@@ -0,0 +1,6 @@
+package com.example.simformcafeteria.adapter
+
+import android.widget.TextView
+
+class ItemDataClass(var timeOfText: String,var timeText: String, var btnToggle: Boolean ,var like: Int, var desLike: Int) {
+}
